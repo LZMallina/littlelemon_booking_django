e3f0f75dac004cc20fd1d579f69cc2007136cc44
lzkrishmom <lzkrishmom@gmail.com>
2023-06-10 01:09:28 -0400

    2nd

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Booking(models.Model):
     first_name = models.CharField(max_length=200)
-    reservation_date = models.DateField()
+    reservation_date = models.DateField(blank=True, null =True)
     reservation_slot = models.SmallIntegerField(default=10)
 
     def __str__(self): 
